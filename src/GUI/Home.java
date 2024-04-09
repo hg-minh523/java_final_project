03008a1e1026f794add02372ccb80c547a9e3078
Khong Tam <khongtam88888888@gmail.com>
2024-04-09 22:17:08 +0700

    gui

--- src/GUI/Home.java
+++ src/GUI/Home.java
@@ -31,6 +31,7 @@ Image leftImg;
         
         initComponents();
     }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
